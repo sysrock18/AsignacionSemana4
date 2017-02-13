b1ff6afc68dad23e7b402fbc589fba2d25e11006
Simon Gonzalez <sgonzalez@galpaservices.com>
2017-02-13 12:15:38 -0400

    Primer commit project

---
+++ README.md
@@ -0,0 +1,27 @@
+# android-3-asignacion-semana-4
+Asigancion semana 4 del modulo 3 del Curso de Android Coursera
+
+Pantalla inicial
+
+![alt tag](https://github.com/sysrock18/AsignacionSemana4/blob/master/Screenshot_1486745251.png)
+
+Perfil
+
+![alt tag](https://github.com/sysrock18/AsignacionSemana4/blob/master/Screenshot_1486745266.png)
+
+Pantalla de favoritos
+
+![alt tag](https://github.com/sysrock18/AsignacionSemana4/blob/master/Screenshot_1486745274.png)
+
+Opciones del menu
+
+![alt tag](https://github.com/sysrock18/AsignacionSemana4/blob/master/Screenshot_1486745283.png)
+
+Pantalla Contacto
+
+![alt tag](https://github.com/sysrock18/AsignacionSemana4/blob/master/Screenshot_1486745288.png)
+
+Pantalla Bio Desarrollador
+
+![alt tag](https://github.com/sysrock18/AsignacionSemana4/blob/master/Screenshot_1486745293.png)
+
---
+++ app/src/main/java/com/example/simongonzalez/asignacionsemana3/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.simongonzalez.asignacionsemana3;
+
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.*;
+import android.widget.Toast;
+
+import com.example.simongonzalez.asignacionsemana3.adapter.PageAdapter;
+import com.example.simongonzalez.asignacionsemana3.fragment.PerfilFragment;
+import com.example.simongonzalez.asignacionsemana3.fragment.RecyclerViewFragment;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        setUpViewPager();
+
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    private ArrayList<Fragment> agregarFragments() {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new RecyclerViewFragment());
+        fragments.add(new PerfilFragment());
+
+        return fragments;
+    }
+
+    private void setUpViewPager() {
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager(), agregarFragments()));
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.home_ico);
+        tabLayout.getTabAt(1).setIcon(R.drawable.dog_ico);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(item.getItemId()) {
+            case R.id.menu_contacto:
+                Intent intentContacto = new Intent(this, ContactoActivity.class);
+                startActivity(intentContacto);
+                return true;
+            case R.id.menu_acercade:
+                Intent intentAcercaDe = new Intent(this, AcercaDeActivity.class);
+                startActivity(intentAcercaDe);
+                return true;
+            case R.id.star:
+                Intent intentFavMascotas = new Intent(this, FavMascotasActivity.class);
+                startActivity(intentFavMascotas);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
